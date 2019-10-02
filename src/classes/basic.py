a3eda8eba4fecd21437a57d6801e43c16fb61b06
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-02 14:03:21 +0530

    created Animal class

--- src/classes/basic.py
+++ src/classes/basic.py
@@ -1,1 +1,28 @@
 # create a basic Animal class add some getter and setter methods
+
+class Animal:
+    # constructor
+    def __init__(self, name, num_legs):
+        self.name = name
+        self.num_legs = num_legs
+
+    # methods
+
+    # getters
+    def getName(self):
+        return self.name
+
+    def getLegs(self):
+        return self.num_legs
+
+    # setters
+    def setName(self, name):
+        self.name = name
+
+    def set_legs(self, num_legs):
+        self.num_legs = num_legs
+
+
+a = Animal("Bob", 20)
+a.name = "Dave"
+a.age = 23
