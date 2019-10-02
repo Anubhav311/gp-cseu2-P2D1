652ba25cf2cc3d4a081066fcc6b3f8b1f15ade9f
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-02 14:11:52 +0530

    created Dog class

--- src/classes/basic.py
+++ src/classes/basic.py
@@ -22,7 +22,25 @@ class Animal:
     def set_legs(self, num_legs):
         self.num_legs = num_legs
 
+# "is a"
+class Dog(Animal):
+    def __init__(self, breed, num_legs=4):
+        self.name = "Dog"
+        self.breed = breed
+        self.num_legs = num_legs
+
+    # getters and setters
+
+    def getBreed(self):
+        return self.breed
+
+    def setBreed(self, breed):
+        self.breed = breed
+
+    
 
 a = Animal("Bob", 20)
 a.name = "Dave"
 a.age = 23
+d = Dog("Hound")
+print(d.getLegs())
