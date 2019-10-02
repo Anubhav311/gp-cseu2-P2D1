3d8081fe26c3852c01f73fccc1d9851e654434c8
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-02 15:14:01 +0530

    created basic category class

--- src/store/category.py
+++ src/store/category.py
@@ -1,2 +1,9 @@
 # create a category class with a name for now 
 # and build upon it further in future iterations
+
+class Category:
+    def __init__(self, name):
+        self.name = name
+
+    def __str___(self):
+        return "No products available in " + self.name
